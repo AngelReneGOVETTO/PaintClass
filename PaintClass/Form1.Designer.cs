29e3bece184bc3568c95ae8478e634a854de7aa1
Angel <Angel@DESKTOP-F6SPQLO>
2023-03-05 18:22:05 -0600

    Agregar archivos de proyecto.

---
+++ PaintClass/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace PaintClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPintar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.pBPapel = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnBAll = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lblTamanioPincel = new System.Windows.Forms.Label();
+            this.btnElip = new System.Windows.Forms.Button();
+            this.btnRec = new System.Windows.Forms.Button();
+            this.btnLin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBPapel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPintar
+            // 
+            this.btnPintar.Location = new System.Drawing.Point(257, 7);
+            this.btnPintar.Name = "btnPintar";
+            this.btnPintar.Size = new System.Drawing.Size(75, 23);
+            this.btnPintar.TabIndex = 0;
+            this.btnPintar.Text = "Pintar";
+            this.btnPintar.UseVisualStyleBackColor = true;
+            this.btnPintar.Click += new System.EventHandler(this.btnPintar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(163, 7);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 1;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // pBPapel
+            // 
+            this.pBPapel.BackColor = System.Drawing.SystemColors.Control;
+            this.pBPapel.Location = new System.Drawing.Point(73, 62);
+            this.pBPapel.Name = "pBPapel";
+            this.pBPapel.Size = new System.Drawing.Size(718, 377);
+            this.pBPapel.TabIndex = 2;
+            this.pBPapel.TabStop = false;
+            this.pBPapel.Paint += new System.Windows.Forms.PaintEventHandler(this.pBPapel_Paint);
+            this.pBPapel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pBPapel_MouseDown);
+            this.pBPapel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pBPapel_MouseMove);
+            this.pBPapel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pBPapel_MouseUp);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(350, 7);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(441, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(73, 7);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(69, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(19, 185);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 32);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(19, 223);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 32);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(19, 79);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(48, 20);
+            this.txtR.TabIndex = 10;
+            this.txtR.Text = "0";
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(19, 105);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(48, 20);
+            this.txtG.TabIndex = 11;
+            this.txtG.Text = "0";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(19, 134);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(48, 20);
+            this.txtB.TabIndex = 12;
+            this.txtB.Text = "0";
+            // 
+            // btnBAll
+            // 
+            this.btnBAll.Location = new System.Drawing.Point(163, 36);
+            this.btnBAll.Name = "btnBAll";
+            this.btnBAll.Size = new System.Drawing.Size(75, 23);
+            this.btnBAll.TabIndex = 13;
+            this.btnBAll.Text = "Borrar All";
+            this.btnBAll.UseVisualStyleBackColor = true;
+            this.btnBAll.Click += new System.EventHandler(this.btnBAll_Click);
+            // 
+            // lblTamanioPincel
+            // 
+            this.lblTamanioPincel.AutoSize = true;
+            this.lblTamanioPincel.Location = new System.Drawing.Point(432, 44);
+            this.lblTamanioPincel.Name = "lblTamanioPincel";
+            this.lblTamanioPincel.Size = new System.Drawing.Size(35, 13);
+            this.lblTamanioPincel.TabIndex = 14;
+            this.lblTamanioPincel.Text = "label4";
+            // 
+            // btnElip
+            // 
+            this.btnElip.Location = new System.Drawing.Point(0, 261);
+            this.btnElip.Name = "btnElip";
+            this.btnElip.Size = new System.Drawing.Size(76, 26);
+            this.btnElip.TabIndex = 15;
+            this.btnElip.Text = "Elipse";
+            this.btnElip.UseVisualStyleBackColor = true;
+            this.btnElip.Click += new System.EventHandler(this.btnElip_Click);
+            // 
+            // btnRec
+            // 
+            this.btnRec.Location = new System.Drawing.Point(0, 293);
+            this.btnRec.Name = "btnRec";
+            this.btnRec.Size = new System.Drawing.Size(76, 23);
+            this.btnRec.TabIndex = 16;
+            this.btnRec.Text = "Rectangulo";
+            this.btnRec.UseVisualStyleBackColor = true;
+            this.btnRec.Click += new System.EventHandler(this.btnRec_Click);
+            // 
+            // btnLin
+            // 
+            this.btnLin.Location = new System.Drawing.Point(0, 326);
+            this.btnLin.Name = "btnLin";
+            this.btnLin.Size = new System.Drawing.Size(76, 24);
+            this.btnLin.TabIndex = 17;
+            this.btnLin.Text = "Linea";
+            this.btnLin.UseVisualStyleBackColor = true;
+            this.btnLin.Click += new System.EventHandler(this.btnLin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLin);
+            this.Controls.Add(this.btnRec);
+            this.Controls.Add(this.btnElip);
+            this.Controls.Add(this.lblTamanioPincel);
+            this.Controls.Add(this.btnBAll);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pBPapel);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnPintar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBPapel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPintar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.PictureBox pBPapel;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnBAll;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblTamanioPincel;
+        private System.Windows.Forms.Button btnElip;
+        private System.Windows.Forms.Button btnRec;
+        private System.Windows.Forms.Button btnLin;
+    }
+}
+
---
+++ PaintClass/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintClass
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        Graphics foto;
+        Bitmap surface;
+        int x = 0;
+        int y = 0;
+        int sX, sY, cX, cY;
+        int index;
+        int R = 0;
+        int G = 0;
+        int B = 0;
+        int tamanioPincel = 3;
+        bool moviendo = false;
+        Pen pen;
+        bool pintar = false;
+        bool borrar = false;
+        public Form1()
+        {
+            InitializeComponent();
+            papel = pBPapel.CreateGraphics();
+            papel.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            tamanioPincel = trackBar1.Value;
+            R = int.Parse(txtR.Text);
+            G = int.Parse(txtG.Text);
+            B = int.Parse(txtB.Text);
+            pen = new Pen(Color.FromArgb(R, G, B), tamanioPincel);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            surface = new Bitmap(pBPapel.Width,pBPapel.Height);
+            foto = Graphics.FromImage(surface);
+            
+            pBPapel.BackgroundImage = surface;
+            pBPapel.BackgroundImageLayout = ImageLayout.None;
+            foto.Clear(Color.White);
+        }
+        //Evento que se dispara al hacer clic y sostenerlo
+        private void pBPapel_MouseDown(object sender, MouseEventArgs e)
+        {
+            moviendo = true;
+            x = e.X;
+            y = e.Y;
+            cX = e.X;
+            cY = e.Y;
+            //Cambiamos cursor por una crus
+            pBPapel.Cursor = Cursors.Cross;
+        }
+        //Eeento que ocurre cunado movemos nuestros cursor sobre el pictureBox
+        private void pBPapel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moviendo && pintar)
+            {
+                //Llamar funcion para cambiar las propiedades del pincel
+                cambiarPincel(int.Parse(txtR.Text), int.Parse(txtG.Text), int.Parse(txtB.Text));
+                //Dibujar una linea posicion actual hacia donde nos movemos
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                foto.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+            if (moviendo && borrar)
+            {
+                cambiarPincel(255, 255, 255);
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                foto.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+        }
+        //Evento que se dispara al dejar de hacer clic 
+        private void pBPapel_MouseUp(object sender, MouseEventArgs e)
+        {
+            moviendo = false;
+            sX = x - cX;
+            sY = y - cY;
+            if (index==3)
+            {
+                papel.DrawEllipse(pen,cX,cY,sX,sY);
+                foto.DrawEllipse(pen, cX, cY, sX, sY);
+            }
+            if (index == 4)
+            {
+                papel.DrawRectangle(pen, cX, cY, sX, sY);
+                foto.DrawRectangle(pen, cX, cY, sX, sY);
+            }
+            if (index == 5)
+            {
+                papel.DrawLine(pen, cX, cY, sX, sY);
+                foto.DrawLine(pen, cX, cY, sX, sY);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            txtR.Text = 0.ToString();
+            txtG.Text = 0.ToString();
+            txtB.Text = 0.ToString();
+        }
+        //PictureBox que muestra el colorDialog
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            //colorDialog1.ShowDialog() mostramos el colorDialog
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtR.Text = colorDialog1.Color.R.ToString();
+                txtG.Text = colorDialog1.Color.G.ToString();
+                txtB.Text = colorDialog1.Color.B.ToString();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblTamanioPincel.Text = trackBar1.Value.ToString();
+        }
+
+        private void btnPintar_Click(object sender, EventArgs e)
+        {
+            pintar = true;
+            borrar = false;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            pintar = false;
+            borrar = true;
+        }
+
+        private void btnElip_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        private void btnRec_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
+        private void btnLin_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+
+        private void pBPapel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics papel = e.Graphics;
+            Graphics foto = e.Graphics;
+            if (pintar)
+            {
+                if (index == 3)
+                {
+                    papel.DrawEllipse(pen, cX, cY, sX, sY);
+                    foto.DrawEllipse(pen, cX, cY, sX, sY);
+                }
+                if (index == 4)
+                {
+                    papel.DrawRectangle(pen, cX, cY, sX, sY);
+                    foto.DrawRectangle(pen, cX, cY, sX, sY);
+                }
+                if (index == 5)
+                {
+                    papel.DrawLine(pen, cX, cY, sX, sY);
+                    foto.DrawLine(pen, cX, cY, sX, sY);
+                }
+            }
+        }
+
+        private void btnBAll_Click(object sender, EventArgs e)
+        {
+            papel.Clear(Color.White);
+            foto.Clear(Color.White);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Png File (*png) | *.png";
+            sfd.DefaultExt = "png";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                surface.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void cambiarPincel(int R, int G, int B)
+        {
+            pen = new Pen(Color.FromArgb(R, G, B), trackBar1.Value);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }   
+    }
+    //TODO: pasar las figuras a este programa
+    /*PLUS: dibujar figuras con dos puntos
+     * PLUSPLUS: dibujar figuras en tiempo real
+     * TODO: boton para guardar el dibujo en el formato
+     */
+}
